1eca73f815a332b36807a26b3abd3a06a6558021
Zhang Yanbo <Zhang.Yanbo@asu.edu>
2024-11-26 19:49:08 -0500

    update parameters

--- experiments/RL/cart_pole.sh
+++ experiments/RL/cart_pole.sh
@@ -1,5 +1,3 @@
-python run.py --exp_name DiffEvoLargeRaw --method diff_evo --env_name CartPole-v1 --dim_in 4 --dim_out 2 --dim_hidden 128 --n_hidden_layers 2 --num_experiment 2 --scaling 1
-python run.py --exp_name DiffEvoRaw --method diff_evo --env_name CartPole-v1 --num_experiment 2 --scaling 1
-python run.py --exp_name DiffEvoLatent --method diff_evo --env_name CartPole-v1 --latent_dim 2 --num_experiment 2 --scaling 1
-python run.py --exp_name DiffEvoLargeLatent --method diff_evo --env_name CartPole-v1 --latent_dim 2 --dim_in 4 --dim_out 2 --dim_hidden 128 --n_hidden_layers 2 --num_experiment 2 --scaling 1
-python run.py --exp_name CMAES --method cmaes --env_name CartPole-v1 --num_experiment 2 --scaling 1
+python run.py --exp_name DiffEvoLatent --method diff_evo --env_name CartPole-v1 --latent_dim 2 --num_experiment 1 --scaling 1 --T 1
+# python run.py --exp_name DiffEvoRaw --method diff_evo --env_name CartPole-v1 --num_experiment 2 --scaling 1
+# python run.py --exp_name CMAES --method cmaes --env_name CartPole-v1 --num_experiment 2 --scaling 1
--- experiments/RL/mountain_car_continuous.sh
+++ experiments/RL/mountain_car_continuous.sh
@@ -1,4 +1,4 @@
-python run.py --exp_name DiffEvoRaw --method diff_evo --env_name MountainCarContinuous-v0 --num_experiment 1 --dim_in 2 --dim_out 1 --controller_type continuous --T 1 --scaling 10
 python run.py --exp_name DiffEvoLatent --method diff_evo --env_name MountainCarContinuous-v0 --latent_dim 2 --dim_in 2 --dim_out 1 --num_experiment 1 --controller_type continuous --T 1 --scaling 10
-python run.py --exp_name DiffEvoLargeLatent --method diff_evo --env_name MountainCarContinuous-v0 --latent_dim 2 --dim_in 2 --dim_out 1 --dim_hidden 128 --n_hidden_layers 2 --num_experiment 1 --controller_type continuous --T 1 --scaling 10
-python run.py --exp_name CMAES --method cmaes --env_name MountainCarContinuous-v0 --num_experiment 1 --controller_type continuous --dim_in 2 --dim_out 1 --T 1 --scaling 10
+# python run.py --exp_name DiffEvoRaw --method diff_evo --env_name MountainCarContinuous-v0 --num_experiment 1 --dim_in 2 --dim_out 1 --controller_type continuous --T 1 --scaling 10
+# python run.py --exp_name DiffEvoLargeLatent --method diff_evo --env_name MountainCarContinuous-v0 --latent_dim 2 --dim_in 2 --dim_out 1 --dim_hidden 128 --n_hidden_layers 2 --num_experiment 1 --controller_type continuous --T 1 --scaling 10
+# python run.py --exp_name CMAES --method cmaes --env_name MountainCarContinuous-v0 --num_experiment 1 --controller_type continuous --dim_in 2 --dim_out 1 --T 1 --scaling 10
